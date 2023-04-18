a297531c27aacccd94d0533a02ef94350b9b3bc1
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-04-18 20:41:42 +0300

    Add error handling

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
@@ -16,6 +18,16 @@ def user(name):
 @app.route('/user/<int:id>')
 def user_id(id):
     return '<h1>Hello, user your id is %s!</h1>' % id
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
