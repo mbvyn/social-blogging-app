5edb5168e884fbe0def756ebd14f39d4929c6172
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-04-19 18:49:22 +0300

    Add form block and favicon

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import InputRequired
+from wtforms.validators import DataRequired
 
 from datetime import datetime
 
@@ -13,14 +13,19 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-class NameForm(Form):
-    name = StringField('What is your name?', validators=[InputRequired()])
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>')
