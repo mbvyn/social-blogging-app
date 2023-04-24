9115b40972d79640432bbca9fe9d0725eaa813e7
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-04-24 18:17:32 +0300

    Add ORM to view function

--- hello.py
+++ hello.py
@@ -20,7 +20,6 @@ app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-
 db = SQLAlchemy(app)
 
 
@@ -57,13 +56,14 @@ def index():
         user = User.query.filter_by(username=form.name.data).first
         if user is None:
             user = User(username=form.name.data)
-            db.session
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 
 @app.route('/user/<name>')
