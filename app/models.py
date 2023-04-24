03181f1b3443f7a619449232680e5b45b7f6898f
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-04-24 17:58:44 +0300

    Rearrange files

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
 
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
