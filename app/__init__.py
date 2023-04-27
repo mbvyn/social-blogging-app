a4f0bd2141aa97207507ad5a9d39079a40afd3fb
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-04-27 22:44:43 +0300

    Implementing functionality in Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -21,4 +21,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
 
+    from main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     return app
