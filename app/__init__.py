c500821ef551b21521a949f37b28c3fb9851d60f
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-04-28 14:37:26 +0300

    Fix package file

--- app/__init__.py
+++ app/__init__.py
@@ -14,14 +14,14 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    config[config_name].init_app()
+    config[config_name].init_app(app)
 
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
 
-    from main import main as main_blueprint
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
