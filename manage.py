c44c7a010094f219c7ea49c4da2a46910f9df4e0
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-04-28 14:37:05 +0300

    Add cli command for running tests

--- manage.py
+++ manage.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
-
+import click
 from dotenv import load_dotenv
 import os
 
 from app import create_app, db
 from app.models import User, Role
-from flask_script import Manager, Shell
 from flask_migrate import Migrate
 
 load_dotenv()
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
 migrate = Migrate(app, db)
 
 
@@ -20,5 +18,14 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 
 
-if __name__ == '__main__':
-    manager.run()
+@app.cli.command()
+@click.argument('test_names', nargs=-1)
+def test(test_names):
+    """Run the unit tests."""
+    import unittest
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
