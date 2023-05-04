f51f9fdb665c85c06c3546c61270c8cbb6c847ca
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-05-04 21:09:17 +0300

    Create /confirm route

--- app/auth/views.py
+++ app/auth/views.py
@@ -76,3 +76,13 @@ def confirm(token):
     else:
         flash('The confirmation link is invalid or has expired.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
