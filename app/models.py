a65a82a262df89097a8c23c5a5211d8aa68364ef
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-05-08 13:53:45 +0300

    Add user profile section

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
-from flask_login import UserMixin, AnonymousUserMixin
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
-
+from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
-from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Permission:
@@ -75,12 +75,17 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
         if self.role is None:
             if self.email == current_app.config['SB_ADMIN']:
-                self.role = Role.query.filter_by(permisions='Administrator').first()
+                self.role = Role.query.filter_by(name='Administrator').first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
 
@@ -156,6 +161,10 @@ class User(UserMixin, db.Model):
 
     def is_administrator(self):
         return self.can(Permission.ADMIN)
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
 
     def __repr__(self):
         return '<User %r>' % self.username
