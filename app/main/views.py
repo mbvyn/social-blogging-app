0bb4968cf647ae0f8d099e6af6cdecb3d0919467
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-05-10 15:39:22 +0300

    Add possibility to edit any profile for admin

--- app/main/views.py
+++ app/main/views.py
@@ -2,9 +2,10 @@ from flask import render_template, flash, redirect, url_for
 from flask_login import login_required, current_user
 
 from . import main
-from .forms import EditProfileForm
+from .forms import EditProfileForm, EditProfileAdminForm
 from .. import db
-from ..models import User
+from ..decorators import admin_required
+from ..models import User, Role
 
 
 @main.route('/')
@@ -34,3 +35,31 @@ def edit_profile():
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', form=form)
+
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        db.session.commit()
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
