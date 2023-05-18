04bf5aca1bcce2aaa50f67b1b5e3064b98a0e040
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-05-18 09:46:32 +0300

    Add coverage functionality

--- manage.py
+++ manage.py
@@ -1,3 +1,5 @@
+import sys
+
 import click
 from dotenv import load_dotenv
 import os
@@ -7,6 +9,13 @@ from app.models import User, Role, Post
 from flask_migrate import Migrate
 
 load_dotenv()
+
+COV = None
+if os.getenv('FLASK_COVERAGE'):
+    import coverage
+
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -18,12 +27,29 @@ def make_shell_context():
 
 
 @app.cli.command()
+@click.option('--coverage/--no-coverage', default=False,
+              help='Run tests under code coverage.')
 @click.argument('test_names', nargs=-1)
-def test(test_names):
+def test(coverage, test_names):
     """Run the unit tests."""
+    if coverage and not os.getenv('FLASK_COVERAGE'):
+        import subprocess
+        os.environ['FLASK_COVERAGE'] = '1'
+        sys.exit(subprocess.call(sys.argv))
+
     import unittest
     if test_names:
         tests = unittest.TestLoader().loadTestsFromNames(test_names)
     else:
         tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
